611b2ab50dfc3f34a827003ab55d8bffff7ca942
Sushree Manaswini Biswal <biswalsushree000@gmail.com>
2025-02-27 23:04:16 +0530

    Added a new URL route

--- quizz_app/urls.py
+++ quizz_app/urls.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/', lambda request: HttpResponse("Welcome to the Quizz App!")),  # Example route
     # Add your application URLs here
 ]
