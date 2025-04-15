e099dbd04b3c93468c859216f22b224d325635d0
Sushree Manaswini Biswal <biswalsushree000@gmail.com>
2025-04-15 23:25:46 +0530

    First Commit

--- backend/members/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def home(request):
-    return HttpResponse("Welcome to the Members App!")  # New home view
-
-def members(request):
-    return render(request, 'index.html')  # Render the existing index.html template
